cb8c671de711bc1fda2441dd066428d673bb94c9
hyaticua <morgenroete8@gmail.com>
2024-04-16 23:26:04 -0400

    add duration based summary and start at message option

--- summarizer_bot/bot.py
+++ summarizer_bot/bot.py
@@ -4,10 +4,11 @@ import discord
 from message import Message
 from summarizer import Summarizer
 
-from loguru import logger
+import datetime
 
 
-message_limit = 50
+MESSAGE_LIMIT = 200
+DISCORD_MESSAGE_LINK_PREFIX = "https://discord.com/channels/"
 
 
 discord_api_key = os.environ.get("DISCORD_API_KEY")
@@ -26,20 +27,47 @@ async def on_ready():
     print(f"We have logged in as {bot.user}")
 
 
-@bot.slash_command()
-async def summarize(ctx: discord.ApplicationContext, num_messages: int = 10):
-    num_messages = min(num_messages, message_limit)
+def parse_message_from_link(message_link: str):
+    if not message_link.startswith(DISCORD_MESSAGE_LINK_PREFIX):
+        return None
 
+    server_id, channel_id, msg_id = [
+        int(x)
+        for x in message_link.removeprefix(DISCORD_MESSAGE_LINK_PREFIX).split("/")
+    ]
+
+    guild = bot.get_guild(server_id)
+    channel = guild.get_channel_or_thread(channel_id)
+    return channel.get_partial_message(msg_id)
+
+
+@bot.slash_command()
+async def summarize(
+    ctx: discord.ApplicationContext,
+    minutes_ago: int = 30,
+    start_message_link: str = None,
+):
     chan = bot.get_channel(ctx.channel_id)
+
+    start_msg = None
+    start_time = datetime.datetime.now() - datetime.timedelta(minutes=minutes_ago)
+
+    if start_message_link:
+        start_msg = parse_message_from_link(start_message_link)
+        if not start_msg:
+            await ctx.respond("Unable to parse start message")
+            return
+
     if not chan:
         await ctx.respond("Sorry I don't have access to read this channel.")
         return
 
-    raw_messages = await chan.history(limit=num_messages).flatten()
+    raw_messages = await chan.history(
+        limit=MESSAGE_LIMIT, after=start_msg or start_time
+    ).flatten()
     raw_messages.reverse()
 
     messages = []
-
 
     for msg in raw_messages:
         # skip bots and empty messages
@@ -47,13 +75,15 @@ async def summarize(ctx: discord.ApplicationContext, num_messages: int = 10):
             continue
 
         author = msg.author
-        # if not isinstance(author, discord.Member):
-        #     members = await msg.guild.query_members(user_ids=[msg.author.id])
-        #     author = members[0]
+        if not isinstance(author, discord.Member):
+            members = await msg.guild.query_members(user_ids=[msg.author.id])
+            member = members[0]
+            if member:
+                author = member
 
         messages.append(Message.convert(msg, author))
 
-    print(f"summarize request: {num_messages=} {len(raw_messages)=} {len(messages)=}")
+    print(f"summarize request: {len(messages)=} {len(raw_messages)=} {len(messages)=}")
 
     summary = await summarizer.summarize(messages)
     await ctx.respond(summary)
